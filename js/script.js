9fa434c8f2a8e7d0316525cac848192ee9ed22bb
Amateur Root Solution <49888553+AmrosoInfinity@users.noreply.github.com>
2026-03-31 15:13:46 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.querySelector("button");
   const tokenBox = document.getElementById("token-box");
-  const token = "1234567890abcdef"; // backend inject di file HTML
 
-  // Delay tampil token
-  setTimeout(() => {
-    tokenBox.innerText = token;
-    tokenBox.classList.remove("hint");
-    tokenBox.style.background = "#fff";
-    tokenBox.style.color = "#333";
-    tokenBox.style.padding = "12px";
-    tokenBox.style.borderRadius = "8px";
-    tokenBox.style.animation = "fadeIn 1s ease-in";
-  }, 2000);
+  // Token sudah diinject backend ke file HTML
+  const token = tokenBox.dataset.token; // backend isi attribute data-token
+
+  // Ganti hint jadi token setelah backend selesai
+  if (token && token.length > 0) {
+    setTimeout(() => {
+      tokenBox.innerText = token;
+      tokenBox.classList.remove("hint");
+      tokenBox.style.background = "#fff";
+      tokenBox.style.color = "#333";
+      tokenBox.style.padding = "12px";
+      tokenBox.style.borderRadius = "8px";
+      tokenBox.style.animation = "fadeIn 1s ease-in";
+    }, 2000); // delay opsional
+  }
 
   // Copy ke clipboard
   btn.addEventListener("click", () => {
